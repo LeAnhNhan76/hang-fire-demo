8ca55bffacc79e97a74d3d31d8bee3feaf42bf2b
lanhanDEV97 <lenhanasus4@gmail.com>
2020-12-18 14:36:52 +0700

    configure hang fire

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Hangfire.MemoryStorage;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,16 +17,24 @@ namespace Hangfire.Demo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHangfire(config =>
+              config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+              .UseSimpleAssemblyNameTypeSerializer()
+              .UseDefaultTypeSerializer()
+              .UseMemoryStorage());
+
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env
+            , IBackgroundJobClient backgroundJobClient)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -35,6 +44,10 @@ namespace Hangfire.Demo
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            app.UseHangfireDashboard();
+            backgroundJobClient.Enqueue(() => Console.WriteLine("Hello Hangfire job!"));
+            backgroundJobClient.Enqueue(() => Console.WriteLine("Hello Hangfire job 2!"));
         }
     }
 }
